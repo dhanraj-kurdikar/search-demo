645450af5ee17d66059f9a0c3c763605da79ff62
Kurdikar, Dhanraj - Dhanraj <dhanraj.kurdikar@lowes.com>
2020-07-26 22:57:29 +0530

    single object response

--- src/main/java/com/search/GoldenEye/resource/ProductResource.java
+++ src/main/java/com/search/GoldenEye/resource/ProductResource.java
@@ -11,13 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductResource {
 
-  @GetMapping(value = "/products/search")
+  /*@GetMapping(value = "/products/search")
   public ResponseEntity<List<Product>> searchProducts(@RequestParam String searchText) {
     return ResponseEntity.ok(Collections.singletonList(Product.builder()
             .imageUrl("https://avatars3.githubusercontent.com/u/31825558?s=460&v=4")
             .title("Chair")
             .subtitle("wooden chair")
             .build()));
+  }*/
+
+  @GetMapping(value = "/products/search")
+  public ResponseEntity<Product> searchProducts(@RequestParam String searchText) {
+    return ResponseEntity.ok(Product.builder()
+            .imageUrl("https://avatars3.githubusercontent.com/u/31825558?s=460&v=4")
+            .title("Chair")
+            .subtitle("wooden chair")
+            .build());
   }
 
 }
